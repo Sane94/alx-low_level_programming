72410e193e9f5ae6fee3a3a2f74c0a461b1dcf1d
Sane94 <odiithankgod@gmail.com>
2022-07-01 06:29:41 -0700

    Adding 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Sane)
+ * Sanae Valet
+ */
+
+int main (void)
+{
+	puts("/"Programming is like building a puzzle");
+	return (0);
+}
