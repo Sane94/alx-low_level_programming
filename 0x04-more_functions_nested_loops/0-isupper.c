1b94342d3a4bd3cbba1ebe97b8b78a230d1c867c
Sane94 <odiithankgod@gmail.com>
2022-07-08 05:53:30 -0700

    Adding 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check for uppercase
+ *
+ * @c: ASCII code
+ * Return: Always 1 (success)
+ * */
+int _isupper(int c)
+{
+if (c => 97 && c =< 122)
+{
+return (1);
+}
+else
+{
+reture (0);
+}
+_putchar(/n);
+}
