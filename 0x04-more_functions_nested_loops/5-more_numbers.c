3e1053b8772052a268b2058505c367c304582d90
Sane94 <odiithankgod@gmail.com>
2022-07-08 14:51:35 -0700

    Adding 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * more_numbers - nmbr fct
- * Return: no return
+ *  more_numbers -> printing more numbers
+ *  Return: void
  */
 void more_numbers(void)
 {
-int i, j;
-for (i = 1; i <= 10; i++)
+int x, y;
+for (x = 0; x < 10; x++)
 {
-for (j = 0; j <= 14; j+)
+for (y = 0; y < 15; y++)
 {
-if (j >= 10)
-_putchar('1');
-_putchar(j % 10 + '0');
+if (y >= 10)
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
 }
 _putchar('\n');
 }
