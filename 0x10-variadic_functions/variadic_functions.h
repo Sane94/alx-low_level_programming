5d6ca9f0a67f1b1f321881ffada4e9c9f1559fab
Sane94 <odiithankgod@gmail.com>
2022-08-05 02:01:51 -0700

    Adding variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/**
+/*
  * File: variadic_functions.h
- * Auth: Tolulope Fakunle
+ * Auth: Odii Thank-God
  * Desc: Header file containing prototypes for all functions
  * used in the 0x0F-variadic_functions directory.
  */
+
 #include <stdarg.h>
+
 /**
  * struct printer - A new struct type defining a printer.
  * @symbol: A symbol representing a data type.
